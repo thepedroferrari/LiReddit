6e2e3c40c980b4c4e920466ca268d9be09e03d36
thepedroferrari <pedro@pedroferrari.com>
2020-09-20 11:46:43 +0200

    Fix instalation and update ignore

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from '../entities/Post';
+import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   const post = orm.em.create(Post, { title: 'my first post' });
   await orm.em.persistAndFlush(post);
-  await orm.em;
+  console.log('--------------sql 2-----------------')
+  await orm.em.nativeInsert(Post, {title: 'My second post 2'})
 };
 
 main().catch(err => {
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,4 +1,4 @@
-import { Post } from 'entities/Post';
+import { Post } from './entities/Post';
 import { __prod__ } from './constants';
 import { MikroORM } from '@mikro-orm/core';
 import path from 'path';
@@ -9,7 +9,8 @@ export default {
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
   entities: [Post],
-  dbName: 'lireddit',
+  dbName: 'lireddit3',
   type: 'postgresql',
   debug: !__prod__,
+  password: 'postgres'
 } as Parameters<typeof MikroORM.init>[0];
