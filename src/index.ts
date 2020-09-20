65df73f846b79563335ef817b2826b5b6b5461aa
thepedroferrari <pedro@pedroferrari.com>
2020-09-20 12:53:55 +0200

    3915 working setup

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import microConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
-  console.log('--------------sql 2-----------------')
-  await orm.em.nativeInsert(Post, {title: 'My second post 2'})
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: 'my first post' });
+  // await orm.em.persistAndFlush(post);
+
+  const posts = await orm.em.find(Post, {});
+  console.log(posts)
 };
 
 main().catch(err => {
