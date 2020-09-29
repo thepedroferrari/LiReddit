4531bb5031ce6bfd86a1e368cf8c3beab5ef565f
thepedroferrari <pedro@pedroferrari.com>
2020-09-29 18:48:39 +0200

    Set query to get logged in by session cookie

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from 'argon2';
 
 import { MyContext } from '../types';
@@ -33,6 +33,19 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(
+      @Ctx() {req, em}:MyContext
+  ) {
+    // you are not logged in
+    if (!req.session!.userId) {
+      return null
+    }
+
+    const user = await em.findOne(User, { id: req.session!.userId })
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
@@ -106,15 +119,6 @@ export class UserResolver {
         }]
       }
     }
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    console.log(req.session)
-    req.session!.userId = user.id
-    console.log(req.session!.userId)
 
     return {
       user
